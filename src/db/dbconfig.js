20519624c1946424841efaff84fa4f977b2a96c5
Umesh Chandra <umeshsoni44@gmail.com>
2019-10-22 00:10:08 +0530

    db details

--- src/db/dbconfig.js
+++ src/db/dbconfig.js
@@ -11,7 +11,8 @@
 const mongoose = require("mongoose");
 
 const dbURI =
-  "mongodb+srv://username:Passwors@cluster0-vb95k.gcp.mongodb.net/LN?retryWrites=true&w=majority";
+  //"mongodb+srv://username:Passwors@cluster0-vb95k.gcp.mongodb.net/LN?retryWrites=true&w=majority";
+  "mongodb+srv://umesh:V3JC7p61jNe12ZG9@cluster0-vb95k.gcp.mongodb.net/LN?retryWrites=true&w=majority";
 
 const options = {
   reconnectTries: Number.MAX_VALUE,
